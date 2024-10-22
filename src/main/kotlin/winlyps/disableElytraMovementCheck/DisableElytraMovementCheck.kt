c812d69f482fcc04783443f2c81c308cec162496
Winlypse <nts3@o2.pl>
2024-10-22 14:18:04 +0200

    Changes

--- src/main/kotlin/winlyps/disableElytraMovementCheck/DisableElytraMovementCheck.kt
+++ src/main/kotlin/winlyps/disableElytraMovementCheck/DisableElytraMovementCheck.kt
@@ -6,9 +6,14 @@ class DisableElytraMovementCheck : JavaPlugin() {
 
     override fun onEnable() {
         // Plugin startup logic
+        server.worlds.forEach { world ->
+            world.setGameRuleValue("disableElytraMovementCheck", "true")
+        }
+        logger.info("DisableElytraMovementCheck plugin has been enabled.")
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+        logger.info("DisableElytraMovementCheck plugin has been disabled.")
     }
 }
